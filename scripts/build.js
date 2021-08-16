ec109a5b2897b3769c2954b4a37d2af9f948fe2e
John Hooks <bitmachina@outlook.com>
2021-08-16 10:14:24 -0700

    add esbuild

---
+++ scripts/build.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const glob = require("glob");
+const { build } = require("esbuild");
+
+glob(path.resolve(__dirname, "../src/**/*.ts"), function (err, entryPoints) {
+  // files is an array of filenames.
+  // If the `nonull` option is set, and nothing
+  // was found, then files is ["**/*.js"]
+  // err is an error object or null.
+
+  if (err) throw err;
+
+  build({
+    entryPoints,
+    bundle: false,
+    // write: false,
+    outdir: path.resolve(__dirname, "../build"),
+    platform: "node",
+    sourcemap: "inline",
+    // external: [
+    //   "@prisma/client",
+    //   "bcrypt",
+    //   "cookie",
+    //   "date-fns",
+    //   "fastify",
+    //   "fastify-plugin",
+    //   "graphql",
+    //   "graphql-scalars",
+    //   "ioredis",
+    //   "jsonwebtoken",
+    //   "lodash",
+    //   "mercurius",
+    //   "nexus",
+    //   "nexus-prisma",
+    // ],
+  })
+    .then(() => {
+      console.log("build complete");
+    })
+    .catch(() => process.exit(1));
+});
